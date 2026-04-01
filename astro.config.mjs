b50d18af32af67a8b06759ef9c959fef2783782b
Diego Lopes <dihgg@dihgg.com>
2026-04-01 17:41:06 -0300

    feat(sitemaps): adding sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,26 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: process.env.PUBLIC_SITE_URL || 'https://dihgg.com',
-  integrations: [react()],
+  integrations: [
+    react(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'pt-BR',
+        locales: {
+          'pt-BR': 'pt-BR',
+          en: 'en'
+        }
+      }
+    })
+  ],
 
   vite: {
     plugins: [tailwindcss()]
