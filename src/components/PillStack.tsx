60d0672f467376b7ebb42a6917a9e3cc686473c2
Diego Lopes <dihgg@dihgg.com>
2026-06-09 13:29:41 -0300

    feat(components): expand pill stack component to use it as post tags on post page

--- src/components/PillStack.tsx
+++ src/components/PillStack.tsx
@@ -1,6 +1,6 @@
 import { STACK_ITEMS_MAX_COUNT } from "@/lib/constants";
 import { normalize } from "@/lib/utils";
-import Pill from "@/components/Pill";
+import Pill, { type PillProps } from "@/components/Pill";
 import classnames from "classnames";
 
 export type PillStackProps = {
@@ -8,23 +8,32 @@ export type PillStackProps = {
     maxCount?: number;
     tinted?: boolean;
     className?: string;
+    variant?: PillProps['variant'];
 };
 
 export default function PillStack(props: PillStackProps) {
-    const { items, maxCount = STACK_ITEMS_MAX_COUNT, tinted = false, className = "" } = props;
+    const {
+        items,
+        maxCount = STACK_ITEMS_MAX_COUNT,
+        tinted = false,
+        className = "",
+        variant = "tag--small"
+    } = props;
+    
     if (items.length === 0) {
         return null;
     }
+    
     return (
         <ul className={classnames("pill-stack", className)}>
             {items.slice(0, maxCount).map((item) => (
                 <li key={item} className="pill-stack__item">
-                    <Pill icon={normalize(item)} variant="tag--small" tinted={tinted}>{item}</Pill>
+                    <Pill icon={normalize(item)} variant={variant} tinted={tinted}>{item}</Pill>
                 </li>
             ))}
             {items.length > maxCount && (
                 <li className="pill-stack__item" title={items.slice(maxCount).join(", ")}>
-                    <Pill variant="tag--small" tinted={tinted}>+{items.length - maxCount}</Pill>
+                    <Pill variant={variant} tinted={tinted}>+{items.length - maxCount}</Pill>
                 </li>
             )}
         </ul>
